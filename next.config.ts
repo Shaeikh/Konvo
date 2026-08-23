e5cb387bb6c9a951e61d4098ac7e156d48e550f8
Shaeikh <doyouhere4@gmail.com>
2026-08-23 17:22:40 +0500

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: [
-    "*.serveousercontent.com",
-    "crinkle-shaping-creatable.ngrok-free.dev",
-  ],
+  allowedDevOrigins: ["crinkle-shaping-creatable.ngrok-free.dev"],
 };
 
 export default nextConfig;
