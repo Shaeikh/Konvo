080f4bfebca1764dabe7eac27c735e23bc73a482
Shaeikh <doyouhere4@gmail.com>
2026-08-23 17:56:12 +0500

    Remove fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 // @ts-ignore
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ViewTransitions } from "next-view-transitions";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: {
@@ -35,14 +21,7 @@ export default function RootLayout({
     <ViewTransitions>
       <html
         lang="en"
-        className={cn(
-          "h-full",
-          "antialiased",
-          geistSans.variable,
-          geistMono.variable,
-          "font-sans",
-          inter.variable,
-        )}
+        className="h-full antialiased font-sans"
         suppressHydrationWarning
       >
         <body className="min-h-full flex flex-col">
